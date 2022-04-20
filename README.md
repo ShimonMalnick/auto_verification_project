8ebd647acae038592e2fda8c648be0568a5e2f6a
Shimon Malnick <shimonmalnick@gmail.com>
2022-04-20 13:14:00 +0300

    Added script running to readme & added the writeup

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # auto_verification_project
-This repo contains the code for the project in the course "Automatic Verification of Systems" (0368-4178 TAU) taken in the Fall of 2021-2022.\
-The repository is based on the repositories of [Marabou](https://github.com/NeuralNetworkVerification/Marabou) and [FGP](https://github.com/klasleino/fast-geometric-projections) and is licensed accordingly.\
+This repo contains a tool for verifying PyTorch DNN's using the [Marabou](https://github.com/NeuralNetworkVerification/Marabou)
+framework. This was done as art of the project in the course "Automatic Verification of Systems" (0368-4178 TAU) taken in the Fall of 2021-2022.\
+The repository can also be used to reproduce some results from the projects (project can be seen [here](project_writeup.pdf)).
+The repository is based on the repository of [Marabou](https://github.com/NeuralNetworkVerification/Marabou) and 
+contains DNN's from the [FGP](https://github.com/klasleino/fast-geometric-projections) repository as these networks were used for comparison in the project.\
 This repo can be used to verify local robustness of PyTorch Fully connected networks with ReLU activations by using the Marabou framework.
 # Get started
 First clone the repository of  [Marabou](https://github.com/NeuralNetworkVerification/Marabou):
@@ -11,12 +14,23 @@ git clone https://github.com/NeuralNetworkVerification/Marabou
 ```
 
 and build it under its  [Build and Dependencies](https://github.com/NeuralNetworkVerification/Marabou#build-and-dependencies) instructions.
-Then make sure you have python >= 3.7 installed with pip to install the additional dependencies (note that these requirements are relevant for reproducing results and some requirements may be relevant for just using the pytorch tool):
+Then make sure you have python >= 3.7 installed with pip to install the additional dependencies:
 ```
 cd auto_verification_project
 pip install -r requirements.txt
 ```
-To use this repository just as a tool for running verification of PyTorch models, go to the [Verify PyTorch Using Marabou](#verify-pytorch-using-marabou) section. 
-# Reproduce Results for
-
+To use this repository as a tool for running verification of PyTorch models, go to the [Verify PyTorch Using Marabou](#verify-pytorch-using-marabou) section.
 # Verify PyTorch Using Marabou
+To use this repo for verification of pytorch networks using Marabou, make sure your network consists only with 
+[*Linear*](https://pytorch.org/docs/stable/generated/torch.nn.Linear.html) 
+layers and [*ReLU*](https://pytorch.org/docs/stable/generated/torch.nn.ReLU.html) activations. The current script provides support for the network I created in the project. To load your
+model add a corresponding function in [model_loaders.py](model_loaders.py) that returns the loaded model.
+Then run:
+```
+python verify_pytorch_net.py --help
+```
+To see the documentation on the relevant parameters.\
+To reproduce results for *My Own DNN* from the project run:
+```
+python verify_pytorch_net.py both 784
+```
--- verify_pytorch_net.py
+++ verify_pytorch_net.py
@@ -1,26 +1,73 @@
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser, RawTextHelpFormatter
 from easydict import EasyDict
-from model_loaders import *
+from typing import Callable
+import model_loaders
+from tools import torch2marabou_onnx
+import os
 
 
 def parse_args() -> EasyDict:
-    convert_str = 'Convert a pytorch network to onnx for Marabou support. requires the arguments:\ninput_shape\n' \
-                  'out_file_path'
-    verify_str = 'Verify a  pytorch network, requires the arguments: converted_path\ninput_shape'
-    ops = ['convert', 'verify']
-    desc = f"Toolbox for verifying pytorch networks using Marabou. the available operations are: {ops}"
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, description=desc)
+    convert_str = 'Convert a pytorch network to onnx for Marabou support.'
+    verify_str = 'Verify a  pytorch network.'
+    ops = ['convert', 'verify', 'both']
+    desc = f"Toolbox for verifying pytorch networks using Marabou. the available operations are:\n{ops}\nAn Example of " \
+           f"running on my own network from the writeup:\n" \
+           f"python verify_pytorch_net.py both --input_shape 784\n" \
+           f"For more information on a specific operation run:\npython verify_pytorch_net.py <op> -h"
+    input_shape_desc = 'The networks input shape (the networks input has to be flatten), for example for mnist it is 784'
+    net_loader_desc = 'name of model loading function from the model_loaders.py file'
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter, description=desc)
     sp = parser.add_subparsers()
     convert_op = sp.add_parser('convert', help=convert_str)
     convert_op.set_defaults(cmd='convert')
-    convert_op.add_argument('input_shape', type=int, help='The networks input shape (the networks input has to be flatten), for example for mnist it is 784')
+    convert_op.add_argument('input_shape', type=int, help=input_shape_desc)
     convert_op.add_argument('out_file_path', type=str, help='The full path to the converted network saveing location, e.g. ./mnist_net.onnx')
-    convert_op.add_argument('--net_loader', help='name of model loading function for the model_loaders.py file',
-                            default='default_loader')
+    convert_op.add_argument('--net_loader', help=net_loader_desc, default='default_loader')
+
     verify_op = sp.add_parser('verify', help=verify_str)
     verify_op.set_defaults(cmd='verify')
     verify_op.add_argument('net_path', help='path for converted pytorch network (converted using the convert op)')
-    verify_op.add_argument('input_shape', type=int, help='The networks input shape (the networks input has to be flatten), for example for mnist it is 784')
+    verify_op.add_argument('input_shape', type=int, help=input_shape_desc)
+
+    both_op = sp.add_parser('both', help='Run both operations sequentially')
+    both_op.set_defaults(cmd='both')
+    both_op.add_argument('input_shape', type=int, help=input_shape_desc)
+    both_op.add_argument('--net_loader', help=net_loader_desc, default='default_loader')
 
     return EasyDict(vars(parser.parse_args()))
+
+
+def get_net_loader(fn_name: str) -> Callable:
+    load_fn = getattr(model_loaders, fn_name, None)
+    if load_fn is None:
+        raise ValueError('Wrong net_loader function given. Make sure the net_loader is defined in model_loaders.py')
+    return load_fn
+
+
+def run_convert(args):
+    load_fn = get_net_loader(args.net_loader)
+    assert not os.path.isfile(args.out_file_path) and not os.path.isdir(args.out_file_path)
+    torch2marabou_onnx(args, load_fn)
+    return f"Saved converted model succesfully to {args.out_file_path}"
+
+
+def run_verify(args):
+    pass
+
+
+def main(args: EasyDict):
+    if args.cmd == 'convert':
+        run_convert(args)
+    elif args.cmd == 'verify':
+        run_verify(args)
+    elif args.cmd == 'both':
+        run_both(args)
+    else:
+        raise ValueError("Wrong operation given")
+
+
+if __name__ == '__main__':
+    cmd_args = parse_args()
+    main(cmd_args)
+
 
